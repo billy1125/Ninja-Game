5ed99e03adbeab5f6ebdee8be655930a2111c3ff
Cho-Hsun Lu <billy1125@gmail.com>
2022-11-28 10:17:41 +0800

    新增貓咪走路音效

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -17,12 +17,14 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
+            GetComponent<AudioSource>().Play();
             transform.Translate(3, 0, 0);
             //transform.position += new Vector3(3, 0, 0); 
         }
 
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
+            GetComponent<AudioSource>().Play();
             transform.Translate(-3, 0, 0);
         }
     }
@@ -35,12 +37,14 @@ public class Player : MonoBehaviour
     // 當玩家按下畫面左按鍵時，貓咪往左移動「3」
     public void LButtonDown()
     {
+        GetComponent<AudioSource>().Play();
         transform.Translate(-3, 0, 0);
     }
 
     // 當玩家按下畫面右按鍵時，貓咪往右移動「3」
     public void RButtonDown()
     {
+        GetComponent<AudioSource>().Play();
         transform.Translate(3, 0, 0);
     }
 
